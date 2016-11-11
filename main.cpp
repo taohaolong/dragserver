45b1b7f9eefc6f7f691c483222e3e4a396ce04b6
Jacky  Tao <migrant.tao@gmail.com>
2016-11-11 09:36:59 +0800

    create

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright: Netease.Inc
+ *
+ * Author: Jacky Tao
+ *
+ * Date: 2016-11-11
+ *
+ * Description: Dragon Audio&Video Transport Server [Main]
+ */
+
+#include <iostream>
+#include <stdio.h>
+
+
+using namespace std;
+
+
+
+int main(int argc, char* argv[]) {
+
+    printf("Dragon AVideo Server start.\n ");
+
+
+    return 0;
+}
