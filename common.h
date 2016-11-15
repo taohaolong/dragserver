4a0e984ca08039901de9a204eca70a40cc0550f9
Jacky  Tao <migrant.tao@gmail.com>
2016-11-15 10:13:49 +0800

    create common.h

---
+++ common.h
@@ -0,0 +1,12 @@
+//
+// Created by tao Jacky on 11/14/16.
+//
+
+#ifndef DRAGONAVS_COMMON_H_H
+#define DRAGONAVS_COMMON_H_H
+
+#include "log/logger.h"
+
+Logger LOG_Drag;
+
+#endif //DRAGONAVS_COMMON_H_H
