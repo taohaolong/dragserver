c2f4233b11cd37c6fb6ce3e3e00d092fc12b5b95
Jacky  Tao <migrant.tao@gmail.com>
2016-11-15 19:05:23 +0800

    req log

--- main.cpp
+++ main.cpp
@@ -15,10 +15,19 @@
 #include "server/dragserver.h"
 #include "server/dragserverEvent.h"
 
+#include "common.h"
+
 using namespace std;
 
 
 int main(int argc, char *argv[]) {
+
+    // Init log
+//    LOG_Drag.init("debug","drtest.log","./", "./", NULL, NULL, false, false, 1024 * 1024 * 5);
+//
+//    LOG_Drag.debug("%s","test bb Start.");
+//
+//    LOG_Drag.info("%s","test Start.");
 
     // server start
     printf("Dragon AVideo Server start.\n ");
@@ -29,7 +38,7 @@ int main(int argc, char *argv[]) {
 //        server.Start();
 
         setvbuf(stdout, NULL, _IONBF, 0);
-
+//
         run();
     }
     catch (const std::exception &e) {
