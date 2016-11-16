f5ccf2899ce9e2d656c5d8c6e9c50166c680e535
Jacky  Tao <migrant.tao@gmail.com>
2016-11-16 11:13:03 +0800

    修改目录结构，去除log

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-
- g++  main.cpp log/filelock.cpp log/filelock.h log/KmMutex.h log/logger.h  log/logger.cpp  \
-    common.h \
-    server/dragserver.h server/dragserver.cpp server/dragserverEvent.h server/dragserverEvent.cpp \
-     -o bin/svr -levent -g
